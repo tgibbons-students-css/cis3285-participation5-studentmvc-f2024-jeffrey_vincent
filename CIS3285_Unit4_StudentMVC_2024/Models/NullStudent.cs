b0e78fd325f60efcd2b401a54e394bfccfa266a9
JeffreyKayfes <jeffreykayfes@gmail.com>
2024-10-06 16:23:30 -0500

    Learning Activity Part 4 and 5 completed

---
+++ CIS3285_Unit4_StudentMVC_2024/Models/NullStudent.cs
@@ -0,0 +1,9 @@
+﻿namespace CIS3285_Unit4_StudentMVC_2024.Models
+{
+    public class NullStudent : IStudentInterface
+    {
+        public int Id { get; set; } = -1;
+        public string Name { get; set; } = "Null Student";
+        public int Credits { get; set; } = -999;
+    }
+}
